58f1ab1c34151991b4f918226f74cb77e8b515b6
Alexandre Fenyo <alexandre.fenyo@gmail.com>
2024-05-28 18:53:18 +0200

    update

--- webclients/WebClient.swift
+++ webclients/WebClient.swift
@@ -16,13 +16,13 @@ class WebClient {
     private var ignore_ssl: Bool
     
     init(url: String, ignore_ssl: Bool = true, proxy_host: String? = nil, proxy_port: UInt16? = nil) throws {
-        do {
-            self.url = try URL(string: url)
-        } catch {
-            
+   
+        guard let _url = URL(string: url) else {
+            throw WebClientError.generalError
         }
+        self.url = _url
         self.ignore_ssl = ignore_ssl
     }
 
-    
+    // CONTINUER ici pour sauvegarder proxy_host et proxy_port ou les traiter directement dans init
 }
