3afc7ee63b41087d969e6bef8ea8340dbd5ff4f9
Alexandre Fenyo <alexandre.fenyo@gmail.com>
2024-08-09 02:27:48 +0200

    update

--- webclients/Main.swift
+++ webclients/Main.swift
@@ -25,21 +25,42 @@ enum AuthError: Error {
 
 // Usage:
 // ./webclients --help
-// ./webclients --help
 
 @main
-struct MyCommand: ParsableCommand {
-    @Argument(help: "The phrase to repeat.")
-    var phrase: String = "toto"
+// The struct name is used to create the help by ArgumentParser. With WebClient as the struct name, we would get "USAGE: web-client ..."
+struct Webclients: ParsableCommand {
+    // -x, --proxy [protocol://]host[:port]
+    @Option(name: [.customShort("x"), .customLong("proxy")], help: "Use the specified proxy ([protocol://]host[:port]).")
+    var opt_proxy: String? = nil
     
-    @Option(help: "The number of times to repeat 'phrase'.")
-    var count: Int? = nil
+    // -k, --insecure
+    //By default, every secure connection curl makes is verified to be secure before the transfer takes place. This option makes curl skip the verification step and proceed without checking
+    @Flag(name: [.customShort("k"), .customLong("insecure")], help: "By default, every secure connection curl makes is verified to be secure before the transfer takes place. This option makes curl skip the verification step and proceed without checking.")
+    var opt_insecure: Bool = false
+
+    // -u, --user name:password
+    @Option(name: [.customShort("u"), .customLong("user")], help: "Specify the user name and password to use for server authentication (user:password).")
+    var opt_cred: String? = nil
+
+    // -U, --proxy-user <user:password>
+    @Option(name: [.customShort("U"), .customLong("proxy-user")], help: "Specify the user name and password to use for proxy authentication (user:password).")
+    var opt_proxy_cred: String? = nil
+
     
+    @Flag(name: .shortAndLong, help: "Print debugging informations.")
+    var verbose = false
+
+    @Argument(help: "Target url to retrieve ([protocol://]host[:port][/path]).")
+    var url: String
+
     mutating func run() throws {
-        let repeatCount = count ?? 2
-        for _ in 0..<repeatCount {
-            print(phrase)
+        if CommandLine.arguments.count == 1 {
+            let exec_name = CommandLine.arguments[0]
+            print("""
+            XXXUsage: \(exec_name) [options...] <url>
+            \(exec_name): try '\(exec_name) --help' or '\(exec_name) -h' for more information
+            """)
+            return
         }
     }
 }
-
