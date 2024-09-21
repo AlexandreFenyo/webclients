6027074fbbd21f81b86b9b93944d94c424d331ed
Alexandre Fenyo <github@fenyo.net>
2024-09-21 21:41:26 +0200

    update

--- webclients/DataConverter.swift
+++ webclients/DataConverter.swift
@@ -23,29 +23,53 @@ import Foundation
 // String : data en utf8, encod vers nonLossyASCII => nil
 // Solution : passer par ascii sur les 1024 premiers octets (cf. https://html.spec.whatwg.org/multipage/parsing.html#determining-the-character-encoding)
 
-func getCharset(data: Data, response: URLResponse) throws -> String.Encoding? {
-    print("XXXXX: getCharset()")
+// La norme HTTP 1.1 indique que le codage par défaut est ISO-8859-1 (https://www.w3.org/International/articles/http-charset/index.fr)
+// The HTML5 specification encourages web developers to use the UTF-8 character set. (https://www.w3schools.com/Html/html_charset.asp))
 
-    var encoding: String.Encoding?
+// <%@ page contentType="text/html; charset=UTF-8" %>
+// HTML4: <meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1">
+// HTML5: <meta charset="Windows-1252">
 
-    if let encoding = response.textEncodingName {
-        switch encoding {
-        case "utf-8":
-            return String.Encoding.utf8
-        default:
-            break
+public struct HTML {
+    let content: String
+
+    static let charsetNameToCharset: [String: String.Encoding] = ["utf-8": String.Encoding.utf8, "ansi": String.Encoding.ascii, "windows-1252": String.Encoding.ascii, "iso-8859-1": String.Encoding.isoLatin1, "iso-8859-2": String.Encoding.isoLatin2]
+    
+    init(data: Data, response: URLResponse) throws {
+        if response.mimeType != "text/html" {
+            throw WebClientError(kind: .generalError, reason: "invalid mime type: \(String(describing: response.mimeType))")
         }
+        let charset = try Self.getCharset(data: data, response: response)
+        guard let content = String(bytes: data, encoding: charset) else {
+            throw WebClientError(kind: .generalError, reason: "can not convert data to string with encoding \(charset)")
+        }
+        self.content = content
     }
 
-
-
-    let x = String(bytes: data, encoding: .utf8)
-//    let x = String(bytes: data, encoding: .isoLatin1)
-//    let x = String(bytes: data, encoding: .ascii)
-
-    print("chaine: \(x)")
-    
-    print("encoding: \(response.textEncodingName)")
-
-    return encoding
+    // Use only when mime-type is text/html
+    private static func getCharset(data: Data, response: URLResponse) throws -> String.Encoding {
+        // default encoding (from HTML spec)
+        var encoding: String.Encoding = .isoLatin1
+        
+        // use HTTP header if no encoding in the HTML content
+        if let enc_header = response.textEncodingName?.lowercased(), let enc_header_charset = Self.charsetNameToCharset[enc_header] {
+            encoding = enc_header_charset
+            // do not look in HTML content if we have a content-type header (on 2024/09/21, http://google.com returns a header saying Latin1 but the content has a meta tag saying UTF-8, and String(bytes:encoding:) does not work with UTF-8 encoding on the returned content)
+            return encoding
+        }
+        
+        // look for encoding in the HTML content
+        guard let head = String(bytes: data[0..<min(1024, data.indices.last!)], encoding: .ascii) else {
+            return encoding
+        }
+        let regex = /charset=(?<charset>[a-zA-Z0-9-]+)/
+        guard let match = try regex.firstMatch(in: head) else {
+            return encoding
+        }
+        if let encoding = Self.charsetNameToCharset[match.charset.lowercased()] {
+            return encoding
+        }
+        
+        return encoding
+    }
 }
--- webclients/PublicInterface.swift
+++ webclients/PublicInterface.swift
@@ -9,17 +9,13 @@ public struct WebClientError: Error {
 }
 
 func example() async throws {
-    let parsed_url = try ParsedURL("http://fenyo.net/tmp/enc/tst.u8")
+    let parsed_url = try ParsedURL("http://fenyo.net/newweb/cplus/")
     let credentials: CredentialsContainer = ["domotique": (StaticCredentials.login, StaticCredentials.password)]
     let session = try WebClientSession(config: AccessNetworkConfig(credentials: credentials))
     let (data, request, response) = try await session.fetch(target: parsed_url.toTarget())
     guard let data, let request, let response else {
         throw WebClientError(kind: .generalError, reason: "invalid fetch results")
     }
-    print(response)
-    if response.mimeType == "text/html" {
-        print("HTML")
-    }
-        
-    try getCharset(data: data, response: response)
+    let html = try HTML(data: data, response: response)
+    print(html.content)
 }
